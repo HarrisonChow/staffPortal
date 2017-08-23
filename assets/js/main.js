b9d977e90c4574e5ac9c61c597a26ce36ceb07be
Harrison Chow <harrisonchow@Harrisons-MacBook-Pro.local>
2017-08-23 14:31:43 +1000

    mobile layout done

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,36 @@ $(document).ready(function() {
   $('body').find('.parent-btn:first').css('border-radius','5px 5px 0 0');
   $('body').find('.parent-btn:last').css('border-radius','0 0 5px 5px');
   $('body').find('.child-btn:last').css('border-radius','0 0 5px 5px');
+
+  current_width = $(window).width();
+  if(current_width < 768){
+    $("#user-details").removeClass('in');
+    $("#user-text .info-btn").text('SHOW');
+    $("#group-details").removeClass('in');
+    $("#group-text .info-btn").text('SHOW');
+  } else {
+    $("#user-details").addClass('in');
+    $("#user-text .info-btn").text('HIDE');
+    $("#group-details").addClass('in');
+    $("#group-text .info-btn").text('HIDE');
+  }
+  //onResize
+  $(window).resize(function(){
+  var current_width = $(window).width();
+  if(current_width < 768){
+    $("#user-details").removeClass('in');
+    $("#user-text .info-btn").text('SHOW');
+    $("#group-details").removeClass('in');
+    $("#group-text .info-btn").text('SHOW');
+  } else {
+    $("#user-details").addClass('in');
+    $("#user-text .info-btn").text('HIDE');
+    $("#group-details").addClass('in');
+    $("#group-text .info-btn").text('HIDE');
+  }
+  });
 });
+
 function collapsing(x) {
   if ($('body').find('.child-btn:last').parent().attr('class') === 'collapse' && $('body').find('.parent-btn:last').parent().attr('id') === x) {
     $('body').find('.parent-btn:last').css('border-radius','0');
